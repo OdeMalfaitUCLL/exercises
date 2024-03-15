01deb4b20a143abde970d53192cf71b322045e66
OdeMalfaitUCLL <ode.malfait@student.ucll.be>
2024-03-15 10:17:12 +0100

    solved 01 and 02 from regex

--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,21 @@
 # Write your code here
+import re
+def is_valid_password(string):
+    positive_regex = [
+        r".{12}",
+        r"[0-9]",
+        r"[a-z]",
+        r"[A-Z]",
+        r"[+-*/.@]",
+    ]
+    negative_regex = [
+        r"(.)\1{2}",
+        r"(.)(.*\1){3}"
+    ]
+    for regex in positive_regex:
+        if not re.search(regex,string):
+            return False
+    for regex in negative_regex:
+        if re.search(regex,string):
+            return False
+    return True
--- 03-regex/02-capturing/04-scrape-email-addresses/student.py
+++ 03-regex/02-capturing/04-scrape-email-addresses/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+import re
+def scrape_email_addresses(string):
+    return re.findall(r"[0-9a-zA-Z.]+@[0-9a-zA-Z.]+",string)
